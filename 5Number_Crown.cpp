0f40934675b3486dbc6971a2f468b5db89295d1f
Student ApnaCollege <gauravbaghel629@gmail.com>
2025-05-12 09:22:01 +0530

    Solved NUmber Crown

---
+++ 5Number_Crown.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i = 1;
+
+    while(i <= n){
+        int start = i;
+        int val = 1;
+
+        while(start){
+            cout<<val;
+            val++;
+            start--;
+        }
+
+        int space = 2*(n-i);
+        while(space){
+            cout<<" ";
+            space--;
+        }
+        
+        int end = i;
+        while(end){
+            cout<<end;
+            end--;
+        }
+        cout<<endl;
+        i++;
+    }
+
+    return 0;
+}
